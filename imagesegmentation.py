5cb8bf5a64721e719fde72eef76058c5ff21018c
Quinten De Man <quinten.deman@stonybrook.edu>
2020-12-26 12:40:21 -0500

    added coordinate feature to image segmentation algorithm

--- imagesegmentation.py
+++ imagesegmentation.py
@@ -1,19 +1,22 @@
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt
 from PIL import Image
 import kmeans as km
 
 #Image processing
-image = Image.open('image1.jpg')
+image = Image.open('image3.jpg')
 image_array = np.asarray(image)
 height = image_array.shape[0]
 width = image_array.shape[1]
-image_array = image_array.reshape(height*width,3)
+image_array = image_array.reshape(height*width,3)/image_array.max()
 
 #K-means algorithm
-X = image_array
-k = 3
-max_iter = 10
+coor_weight = 0.5
+x_coor = np.transpose(np.tile(np.arange(width,dtype='float32'),height)).reshape(height*width,1)/width*coor_weight
+y_coor = np.transpose(np.repeat(np.arange(height,dtype='float32'),width)).reshape(height*width,1)/height*coor_weight
+X = np.append(np.append(image_array,x_coor,1),y_coor,1)
+k = 4
+max_iter = 5
 
 print('Performing K-means...')
 centroids = km.initialize_centroids(X, k)
@@ -27,7 +30,8 @@ print('Done.')
 closest_centroids = km.find_closest_centroids(X, centroids)
 new_image = np.zeros((height*width,3))
 for i in range(new_image.shape[0]):
-    new_image[i] = centroids[closest_centroids[i]]
+    new_image[i] = centroids[closest_centroids[i]][0:3]
     
 new_image = new_image.reshape(height,width,3)
-cv2.imshow('output', new_image/256)
+plt.imshow(new_image)
+plt.show()
