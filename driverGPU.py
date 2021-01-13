ca0e82bfa0c006ceb98a93b085a95878c993f10e
Quinten De Man <quinten.deman@stonybrook.edu>
2021-01-13 16:11:11 -0500

    added a feature to the data for time in the video

--- driverGPU.py
+++ driverGPU.py
@@ -14,15 +14,18 @@ def main():
     video_array = vp.video_to_array(path,target_resolution,target_fps)
     print('Done.')
 
-    #K-means algorithm
-    X = torch.from_numpy(video_array).to(torch.device('cuda'))
-    k = 10
-    max_iter = 10
+    #Data initialization
+    time_weight = 100
     
-    #Feature normalization
+    X = torch.from_numpy(video_array).to(torch.device('cuda'))
     #X = km.scale_features(X,0)
     X = km.scale_features(X,1)
+    X = km.add_position_feature(X,time_weight)
 
+    #K-means algorithm
+    k = 10
+    max_iter = 20
+    
     print('Performing K-means...')
     centroids = km.initialize_centroids(X, k)
     for i in range(max_iter):
@@ -31,14 +34,20 @@ def main():
         centroids = km.update_centroids(X, k, closest_centroids)
     print('Done.')
 
+    print(closest_centroids)
+
     #display centroids and thumbnails
     closest_points = km.find_closest_points(X, centroids)
+    X = X[:,0:X.shape[1]-1]
+    centroids = centroids[:,0:centroids.shape[1]-1]
     for i in range(k):
         image = centroids[i,:].reshape(target_resolution[0],target_resolution[1],3)
         plt.imshow(cv2.cvtColor(image.cpu().numpy(),cv2.COLOR_BGR2RGB))
+        plt.title('centroid '+str(i))
         plt.show()
         image = X[closest_points[i],:].reshape(target_resolution[0],target_resolution[1],3)
         plt.imshow(cv2.cvtColor(image.cpu().numpy(),cv2.COLOR_BGR2RGB))
+        plt.title('thumbnail '+str(i))
         plt.show()
 
 if __name__ == '__main__':
--- kmeansGPU.py
+++ kmeansGPU.py
@@ -4,14 +4,20 @@ import torch
 
 device = torch.device('cuda')
 dtype = torch.float32
-epsilon = torch.tensor(0.0001,dtype=torch.float32)
 
+#Scales the features between 0 and 1 along the row (axis=1) or column (axis=0)
 def scale_features (X,axis):
+    epsilon = torch.tensor(0.0001,dtype=torch.float32)
     if axis == 0:
         return (X-X.min(0).values)/((X-X.min(0).values).max(0).values+epsilon)
     elif axis == 1:
         return (X-X.min(1).values.reshape((X.shape[0],1)))/((X-X.min(1).values.reshape((X.shape[0],1))).max(1).values.reshape((X.shape[0],1))+epsilon)
 
+#Add a feature to the data of the time in the video (represented by row index in the tensor)
+def add_position_feature (X,weight):
+    return torch.cat((X,weight*(torch.arange(X.shape[0],dtype=torch.float32).to(device).reshape((X.shape[0],1))/X.shape[0])),dim=1)
+
+#Initialize the centroids to random data points return the initialized centroids
 def initialize_centroids (X,k):
     centroids = torch.empty((k,X.shape[1]), device=device, dtype=dtype)
     indices = r.sample(range(X.shape[0]),k)
@@ -19,6 +25,7 @@ def initialize_centroids (X,k):
         centroids[i,:] = X[indices[i],:]
     return centroids
 
+#Return a tensor of centroid indices that each data point is closest to (which cluster they belong to)
 def find_closest_centroids (X,centroids):
     k = centroids.shape[0]
     distances = torch.empty((X.shape[0],k), device=device, dtype=dtype)
@@ -27,6 +34,7 @@ def find_closest_centroids (X,centroids):
     closest_centroids = torch.argmin(distances, dim=1).to(device)
     return closest_centroids
 
+#Update the centroids to the mean position of all data points in their cluster
 def update_centroids (X,k,closest):
     centroids = torch.empty((k,X.shape[1]), device=device, dtype=dtype)
     for i in range(k):
@@ -37,6 +45,7 @@ def update_centroids (X,k,closest):
             centroids[i,:] = X[r.sample(range(X.shape[0]),1),:]
     return centroids
 
+#Get the data point in every cluster that is closest to the centroid
 def find_closest_points (X,centroids):
     k = centroids.shape[0]
     distances = torch.empty((X.shape[0],k), device=device, dtype=dtype)
